b811aad3d7f4913f492892ba35dba0c7057eee0c
anastastelios <anastas.stelios@gmail.com>
2015-10-13 01:11:59 +0300

    Update and rename trialset.m to trialset_stelios.m

--- trialset_stelios.m
+++ trialset_stelios.m
@@ -19,8 +19,26 @@ number=[1:12]';
 trials=struct()
 
 for i=1:12
-trials(i).number=number(i)
-trials(i).type=typeS(i)
+    trials(i).number=number(i)
+    trial_index = typeS(i)
+    if (~(i--ypoloipo--/4))
+        check_index = 0;
+        while super_index < 3
+            for j = 1:(i--ypoloipo--/4-1)
+                if trial_index ~= trials(i-j)
+                    check_index = check_index + 1;
+                end
+            end
+    
+            if check_index == j
+                trials(i).type = trial_index;
+                super_index = 5;
+            else
+                super_index = 1;
+            end
+        end
+    end
+                
 end
 
 set=randperm(4);
